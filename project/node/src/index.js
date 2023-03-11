997ef6c006eb033032d3f9dcaf1a1c50d5619d67
Eduardo Villas Boas <eduardovillasboassantos@gmail.com>
2023-03-11 18:56:12 -0300

    feat: everything working, but waiting the response from suport team

--- project/node/src/index.js
+++ project/node/src/index.js
@@ -3,10 +3,59 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db_app', //variável de ambiente vindo do docker-compose
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `insert into people(name) values ('eduardo')`
+connection.query(sql)
+
+class People {
+    constructor(id, name) {
+        this.id = id
+        this.name = name
+    }
+}
+
+function getAllPeople(sendResult) {
+
+    const getAllPeopelSql = 'select * from people'
+    connection.query(getAllPeopelSql, (_err, result, _fields) => {
+        let peopleList = []
+        peopleList.push(
+            new People(
+                1,
+                "Eduardo"
+            )
+        )
+        sendResult(result)                
+    })
+
+
+}
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    getAllPeople(list => {
+        const response = `<h1>Full Cycle Rocks!</h1><ul>${list.map(p => `<li>Id: ${p.id}, Name: ${p.name}</li>`).join('')}</ul>`
+        res.send(response)
+    })
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listen on port: ${port}`)
 })
+
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.')
+    console.log('Closing http server.')
+    server.close(() => {
+      console.log('Http server closed.')
+      connection.end()
+    });
+  });
